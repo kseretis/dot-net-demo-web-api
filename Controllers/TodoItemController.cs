0b8c7aa510bcc59765382ac5ea778b579e443fae
kseretis <serkleanthis@gmail.com>
2023-10-05 15:21:22 +0300

    Add project files.

---
+++ Controllers/TodoItemController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dot_net_demo_web_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodoItemController : ControllerBase
+    {
+        private readonly ILogger<TodoItemController> _logger;
+        private TodoItem _item;
+
+        public TodoItemController(ILogger<TodoItemController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTodoItem")]
+        public IEnumerable<TodoItem> Get()
+        {
+            return Enumerable.Range(1, 3).Select(index => new TodoItem
+            {
+                Id = index,
+                Name = "Test" + Random.Shared.Next(-20, 50),
+                IsComplete = false
+            })
+            .ToArray();
+        }
+
+        [HttpPost(Name = "PostTodoItem")]
+        public TodoItem Post(TodoItem todoItem)
+        {
+            _item = todoItem;
+            Console.WriteLine("item name: " + _item.Name);
+
+            return _item;
+        }
+    }
+
+}
---
+++ TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace dot_net_demo_web_api
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+
+}
